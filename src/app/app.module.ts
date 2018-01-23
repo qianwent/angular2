1afc4f1a4ab01ce735ffad8cddcd6bcac23a1b18
Will Qian <qianwent@hotmail.com>
2018-01-23 18:58:02 -0500

    extract user detail as an individual component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent //generated automatically with CLI
+    UsersComponent,
+    UserDetailComponent //generated automatically with CLI
   ],
   imports: [
     BrowserModule,
